59f8692bd1213a9f86004506b0a8f957079e916d
Friedel Ziegelmayer <friedel.ziegelmayer@gmail.com>
2013-03-30 16:41:16 +0100

    Simplify merge of data with options.

--- tasks/grunt-karma.js
+++ tasks/grunt-karma.js
@@ -10,14 +10,14 @@ var runner = require('karma').runner;
 var server = require('karma').server;
 
 module.exports = function(grunt) {
+  var _ = grunt.util._;
+
   grunt.registerMultiTask('karma', 'run karma.', function() {
     var done = this.async();
     var options = this.options();
     var data = this.data;
     //merge options onto data, with data taking precedence
-    Object.keys(this.options()).forEach(function(prop){
-      if (!data[prop]) data[prop] = options[prop];
-    });
+    data = _.merge(options, data);
 
     if (data.configFile) {
       data.configFile = grunt.template.process(data.configFile);
