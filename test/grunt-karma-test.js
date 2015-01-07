052b329f96eec201e7d44743ac3f0ebc5459a8f9
dignifiedquire <dignifiedquire@gmail.com>
2015-01-07 23:24:41 +0100

    refactor(task): ensure jscs and jshint are happy

--- test/grunt-karma-test.js
+++ test/grunt-karma-test.js
@@ -1,14 +1,14 @@
-describe('grunt-karma', function(){
+describe('grunt-karma', function() {
 
-  describe('one', function(){
-    it('should be awesome', function(){
+  describe('one', function() {
+    it('should be awesome', function() {
       console.log('one');
       expect('foo').to.be.a('string');
     });
   });
 
-  describe('two', function(){
-    it('should be equally awesome', function(){
+  describe('two', function() {
+    it('should be equally awesome', function() {
       console.log('two');
       expect('woot').to.be.a('string');
     });
