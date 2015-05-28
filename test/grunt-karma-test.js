d1015845e48bfc365317ca91d5903e41d3498088
dignifiedquire <dignifiedquire@gmail.com>
2015-05-28 21:44:10 +0200

    chore: Move to eslint and standard.
    No more coffeescript, jscs or jshint.

--- test/grunt-karma-test.js
+++ test/grunt-karma-test.js
@@ -1,17 +1,17 @@
-describe('grunt-karma', function() {
+/* global describe, it, expect */
 
-  describe('one', function() {
-    it('should be awesome', function() {
-      console.log('one');
-      expect('foo').to.be.a('string');
-    });
-  });
+describe('grunt-karma', function () {
+  describe('one', function () {
+    it('should be awesome', function () {
+      console.log('one')
+      expect('foo').to.be.a('string')
+    })
+  })
 
-  describe('two', function() {
-    it('should be equally awesome', function() {
-      console.log('two');
-      expect('woot').to.be.a('string');
-    });
-  });
-
-});
+  describe('two', function () {
+    it('should be equally awesome', function () {
+      console.log('two')
+      expect('woot').to.be.a('string')
+    })
+  })
+})
